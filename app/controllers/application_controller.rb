cc406bc9aaaa32c009ec8f239c6c0eda273387ad
Chris Hughes <chris@lokku.com>
2015-06-05 14:20:44 +0100

    Add helper method to ApplicationController to list all countries

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,12 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  helper_method :countries
+
+  ## Return array of all supported countries
+  def countries
+    [ 'au', 'br', 'de', 'es', 'fr', 'in', 'it', 'mx', 'uk' ]
+  end
+
 end
